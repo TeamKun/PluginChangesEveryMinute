b601e11168188afd9fb81381e5a3bf30dd8a043f
maru-256 <maru043@docomo.ne.jp>
2021-11-12 00:32:16 +0900

    add PluginProperty

---
+++ src/main/java/net/kunmc/lab/paperplugintemplate/PluginProperty.java
@@ -0,0 +1,65 @@
+package net.kunmc.lab.paperplugintemplate;
+
+import net.kunmc.lab.config.BaseConfig;
+import net.kunmc.lab.value.BooleanValue;
+import net.kunmc.lab.value.StringListValue;
+import net.kunmc.lab.value.StringValue;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class PluginProperty extends BaseConfig {
+    private final StringValue pluginName;
+    public final StringValue projectName;
+    public final StringValue explanation;
+    private final StringListValue startUpCommands;
+    public final BooleanValue shouldUse = new BooleanValue(true);
+
+    public PluginProperty(Plugin plugin, String pluginName, String projectName, String explanation, List<String> startUpCommands) {
+        super(plugin, pluginName);
+        this.pluginName = new StringValue(pluginName)
+                .writableByCommand(false)
+                .listable(false);
+        this.projectName = new StringValue(projectName)
+                .writableByCommand(false);
+        this.explanation = new StringValue(explanation)
+                .writableByCommand(false);
+        this.startUpCommands = new StringListValue(startUpCommands)
+                .addableByCommand(false)
+                .removableByCommand(false)
+                .clearableByCommand(false);
+
+        this.saveConfigIfAbsent();
+    }
+
+    public boolean pluginExists() {
+        return Bukkit.getPluginManager().getPlugin(pluginName.value()) != null;
+    }
+
+    @Nullable
+    public Plugin getPlugin() {
+        return Bukkit.getPluginManager().getPlugin(pluginName.value());
+    }
+
+    public boolean pluginEnabled() {
+        if (pluginExists()) {
+            return getPlugin().isEnabled();
+        }
+
+        return false;
+    }
+
+    public void disablePlugin() {
+        if (pluginExists()) {
+            Bukkit.getPluginManager().disablePlugin(getPlugin());
+        }
+    }
+
+    public void dispatchCommands() {
+        for (String command : startUpCommands) {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+        }
+    }
+}
