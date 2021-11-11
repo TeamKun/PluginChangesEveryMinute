c3e9e80633f798c571499047eca15bc3a000243f
maru-256 <maru043@docomo.ne.jp>
2021-11-12 02:09:34 +0900

    add loading config logic

--- src/main/java/net/kunmc/lab/paperplugintemplate/PaperPluginTemplate.java
+++ src/main/java/net/kunmc/lab/paperplugintemplate/PaperPluginTemplate.java
@@ -1,10 +1,20 @@
 package net.kunmc.lab.paperplugintemplate;
 
+import net.kunmc.lab.config.BaseConfig;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public final class PaperPluginTemplate extends JavaPlugin {
+    final List<PluginProperty> pluginPropertyList = new ArrayList<>();
+
     @Override
     public void onEnable() {
+        for (File file : getDataFolder().listFiles()) {
+            pluginPropertyList.add(BaseConfig.newInstanceFrom(file, PluginProperty.class, this));
+        }
     }
 
     @Override
